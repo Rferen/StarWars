a00cea1a46c0e32b28ca761b156aec4b7fdc883d
Tamás <dotomiro2@gmail.com>
2023-11-27 19:13:47 +0100

    Add new task, hints for fetch request

--- app.js
+++ app.js
@@ -1,30 +1,6 @@
 const cardContainer = document.querySelector('#card-container');
-const characters = [
-    {
-      id: 1,
-      name: 'Luke Skywalker',
-      video: './videos/luke.mp4',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi3koTbzJJDLaPGoPOIFnP8woGx1uWnDfFwl5SuMNuP29zpcsXN6jDWJbr4b2PfjIt_sQ&usqp=CAU',
-    },
-    {
-      id: 2,
-      name: 'Darth Vader',
-      video: './videos/darth-vader.mp4',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8CagerwTzyLVcbO4J8-cUa6qnxOKyFHX7wkjb-bIJIF6uVE7vF51hU7JMfJ6S_lhZTv0&usqp=CAU',
-    },
-    {
-      id: 3,
-      name: 'Yoda',
-      video: './videos/yoda-dies.mp4',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAz-zBlMM3FG-RJVmbzXVEBzcCF1B6o67yfcWd9ONl5efASOs1IGnDOMHtJcgjkdtBn28&usqp=CAU',
-    },
-    {
-      id: 4,
-      name: 'Palpatine',
-      video: './videos/palpatine.mp4',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDcpaktCWrELo5qwJJ-rEzvCZJQz0IC2oJp4XtEZe8jtqKjnMRlTLybF9_RhPeDa3MrXY&usqp=CAU',
-    },
-  ];
+
+async function main() {
   for (let i = 0; i < characters.length; i++) {
     const cardTemplate = `
       <div class="card">
@@ -37,17 +13,47 @@ const characters = [
       </div>
     `;
     cardContainer.innerHTML += cardTemplate;
+  }
 }
+
+/* const characters = [
+  {
+    id: 1,
+    name: 'Luke Skywalker',
+    video: './videos/luke.mp4',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi3koTbzJJDLaPGoPOIFnP8woGx1uWnDfFwl5SuMNuP29zpcsXN6jDWJbr4b2PfjIt_sQ&usqp=CAU',
+  },
+  {
+    id: 2,
+    name: 'Darth Vader',
+    video: './videos/darth-vader.mp4',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8CagerwTzyLVcbO4J8-cUa6qnxOKyFHX7wkjb-bIJIF6uVE7vF51hU7JMfJ6S_lhZTv0&usqp=CAU',
+  },
+  {
+    id: 3,
+    name: 'Yoda',
+    video: './videos/yoda-dies.mp4',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAz-zBlMM3FG-RJVmbzXVEBzcCF1B6o67yfcWd9ONl5efASOs1IGnDOMHtJcgjkdtBn28&usqp=CAU',
+  },
+  {
+    id: 4,
+    name: 'Palpatine',
+    video: './videos/palpatine.mp4',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDcpaktCWrELo5qwJJ-rEzvCZJQz0IC2oJp4XtEZe8jtqKjnMRlTLybF9_RhPeDa3MrXY&usqp=CAU',
+  },
+]; */
 
 function showVideos(index) {
-    const videoContainer = document.getElementById(`videoContainer${index}`);
+  const videoContainer = document.getElementById(`videoContainer${index}`);
 
-    // Ellenőrizzük, hogy a videókonténer látható-e vagy sem
-    if (videoContainer.style.display === 'none') {
-        // Ha nem látható, akkor megjelenítjük
-        videoContainer.style.display = 'block';
-    } else {
-        // Ha látható, akkor elrejtjük
-        videoContainer.style.display = 'none';
-    }
+  // Ellenőrizzük, hogy a videókonténer látható-e vagy sem
+  if (videoContainer.style.display === 'none') {
+      // Ha nem látható, akkor megjelenítjük
+      videoContainer.style.display = 'block';
+  } else {
+      // Ha látható, akkor elrejtjük
+      videoContainer.style.display = 'none';
+  }
 }
+
+main();
